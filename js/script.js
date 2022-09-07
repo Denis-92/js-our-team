35432f1aa86fad8fd19dd85bfbc143a4921ba233
Denis-92 <d.mozenic@gmail.com>
2022-09-07 10:30:54 +0200

    corretto object dentro array

--- js/script.js
+++ js/script.js
@@ -23,37 +23,37 @@ Prima la logica in italiano e poi traduciamo in codice.
 // Creare l’array di oggetti con le informazioni fornite.
 const teamMembers = [
 
-    barnett = {
+    {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
         foto: 'wayne-barnett-founder-ceo.jpg'
     },
 
-    caroll = {
+    {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
         foto: 'angela-caroll-chief-editor.jpg'
     },
 
-    gordon = {
+    {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
         foto: 'walter-gordon-office-manager.jpg'
     },
 
-    lopez = {
+    {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
         foto: 'angela-lopez-social-media-manager.jpg'
     },
 
-    estrada = {
+    {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
         foto: 'scott-estrada-developer.jpg'
     },
 
-    ramos = {
+    {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         foto: 'barbara-ramos-graphic-designer.jpg'
